db14910f93e6c3c60df68223a799546e26a10a40
afra-ibrahim1996 <afra23466@gmail.com>
2023-08-22 17:02:58 -0700

    printf project

--- specifier.c
+++ specifier.c
@@ -48,7 +48,7 @@ int (*get_specifier(char *s))(va_list ap, params_t *params)
 */
 int get_print_func(char *s, va_list ap, params_t *params)
 {
-	int (*f)(va_list ap, params_t *) = get_specifier(s);
+	int (*f)(va_list, params_t *) = get_specifier(s);
 
 	if (f)
 		return (f(ap, params));
